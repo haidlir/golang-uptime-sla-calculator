942f6c983511f1ecf53897f312e0741887ba5832
haidlir <haidlir@gmail.com>
2019-11-11 11:23:10 +0700

    New Lib:
    - Bakti 1 Availbility
    - Bakti 1 Chronology (Running, Link Failure, Power Failure, Open)

---
+++ sla-calculator/sla_calculator_explainer.go
@@ -0,0 +1,122 @@
+package slacalculator
+
+const (
+	// BaktiRunning is the status definition of running services
+	BaktiRunning = 1
+	// BaktiLinkFailure is the status definition of down services caused by link failure
+	BaktiLinkFailure = 2
+	// BaktiPowerFailure is the status definition of down services caused by power failure
+	BaktiPowerFailure = 3
+	// BaktiOpen is the status definition of down services caused by no unkown reason (open)
+	BaktiOpen = 4
+)
+
+// Bakti1UptimeChronology explains the chronolgy of each interval uptime data
+type Bakti1UptimeChronology struct {
+	StartTimestamps int64
+	EndTimestamps   int64
+	UptimeValue     int64
+	Status          int
+}
+
+func transformSomething(timestamps []int64, uptimeValues []int64) (countedVals []int64) {
+	// Copy uptimeValues to countedVals
+	countedVals = append(countedVals, uptimeValues...)
+	maxIteration := len(uptimeValues)
+	var i, j int = 0, 0
+	for (i < maxIteration) && (j < maxIteration) {
+		if uptimeValues[i] == 0 {
+			i++
+			continue
+		}
+		if j != i {
+			for k := i; countedVals[k] > 0 && k > j; k-- {
+				countedVals[k-1] = countedVals[k] - (timestamps[k] - timestamps[k-1])
+			}
+			// normalisasi index i dan j
+			j = i
+		}
+		i++
+		j++
+	}
+	return countedVals
+}
+
+// ExplainBakti1Uptime explains the status of service refers to uptime data from BAKTI series.
+func (u *UptimeSLACalculator) ExplainBakti1Uptime() []Bakti1UptimeChronology {
+	timestamps := []int64{}
+	uptimeValues := []int64{}
+	if u.startTime != u.timestamps[0] {
+		timestamps = append(timestamps, u.startTime)
+		uptimeValues = append(uptimeValues, int64(0))
+	}
+	timestamps = append(timestamps, u.timestamps...)
+	uptimeValues = append(uptimeValues, u.uptimeValues...)
+	if u.endTime != u.timestamps[len(u.timestamps)-1] {
+		timestamps = append(timestamps, u.endTime)
+		uptimeValues = append(uptimeValues, int64(0))
+	}
+	countedVals := transformSomething(timestamps, uptimeValues)
+	chronologies := []Bakti1UptimeChronology{}
+	for i := 0; i < len(countedVals); i++ {
+		if i == len(countedVals)-1 {
+			break
+		}
+		var status int
+		if uptimeValues[i] > 0 {
+			status = BaktiRunning
+		} else if countedVals[i] > 0 && uptimeValues[i] == 0 {
+			status = BaktiLinkFailure
+		} else if countedVals[i] <= 0 {
+			status = BaktiPowerFailure
+		}
+		chronology := Bakti1UptimeChronology{
+			StartTimestamps: timestamps[i],
+			EndTimestamps:   timestamps[i+1],
+			UptimeValue:     uptimeValues[i],
+			Status:          status,
+		}
+		chronologies = append(chronologies, chronology)
+	}
+	// Open Status Correction from start
+	for i := 0; i < len(chronologies) && countedVals[i] == 0; i++ {
+		chronologies[i].Status = BaktiOpen
+	}
+	// Open Status Correction from end
+	for i := len(chronologies) - 1; i > 0 && countedVals[i] == 0; i-- {
+		chronologies[i].Status = BaktiOpen
+	}
+	return chronologies
+}
+
+// Bakti1Availability explains the chronolgy of each interval uptime data
+type Bakti1Availability struct {
+	Availability        float64
+	LinkFailureDuration int64
+	OpenDuration        int64
+	Chronologies        []Bakti1UptimeChronology
+}
+
+// CalcBakti1Uptime returns the SLA and explains the status of service refers to uptime data from BAKTI series.
+func (u *UptimeSLACalculator) CalcBakti1Uptime() *Bakti1Availability {
+	chronologies := u.ExplainBakti1Uptime()
+	// Calc Availability
+	periodDuration := u.endTime - u.startTime
+	var linkFailureDuration int64
+	var openDuration int64
+	for _, chronology := range chronologies {
+		if chronology.Status == BaktiLinkFailure {
+			linkFailureDuration += chronology.EndTimestamps - chronology.StartTimestamps
+		} else if chronology.Status == BaktiOpen {
+			openDuration += chronology.EndTimestamps - chronology.StartTimestamps
+		}
+	}
+	availability := 1.0 - (float64(linkFailureDuration+openDuration) / float64(periodDuration))
+	baktiAvailability := Bakti1Availability{
+		Availability:        availability,
+		LinkFailureDuration: linkFailureDuration,
+		OpenDuration:        openDuration,
+		Chronologies:        chronologies,
+	}
+	return &baktiAvailability
+}
---
+++ sla-calculator/sla_calculator_explainer_test.go
@@ -0,0 +1,175 @@
+package slacalculator_test
+
+import (
+	"math"
+	"testing"
+
+	slacalc "github.com/haidlir/golang-uptime-sla-calculator/sla-calculator"
+)
+
+var (
+	startTimeBakti int64 = 10000
+	endTimeBakti   int64 = 13000
+)
+
+var uptimeBaktiSeriesData = []UptimeData{
+	{10000, 0, false}, // open
+	{10100, 0, false}, // open
+	{10200, 0, false}, // power failure
+	{10300, 0, false}, // link failure
+	{10400, 0, false}, // link failure
+	{10500, 270, false},
+	{10600, 370, false},
+	{10700, 470, false},
+	{10800, 570, false},
+	{10900, 670, false},
+	{11000, 40, false},
+	{11100, 140, false},
+	{11200, 240, false},
+	{11300, 340, false},
+	{11400, 440, false},
+	{11500, 540, false},
+	{11600, 0, false}, // link failure
+	{11700, 0, false}, // link failure
+	{11800, 840, false},
+	{11900, 940, false},
+	{12000, 1040, false},
+	{12100, 1140, false},
+	{12200, 0, false}, // power failure
+	{12300, 0, false}, // power failure
+	{12400, 0, false}, // power failure
+	{12500, 10, false},
+	{12600, 110, false},
+	{12700, 210, false},
+	{12800, 0, true}, // open
+	{12900, 0, true}, // open
+	{13000, 0, true}, // no next
+}
+
+func TestExplainBakti1Uptime(t *testing.T) {
+	// form each array
+	{
+		endTime := endTimeBakti
+		uptimeVals := []int{}
+		timestamps := []int64{}
+		exceptions := []bool{}
+		for _, val := range uptimeBaktiSeriesData {
+			uptimeVals = append(uptimeVals, val.Value)
+			timestamps = append(timestamps, val.Timestamp)
+			exceptions = append(exceptions, val.Exception)
+		}
+		calc, err := slacalc.NewUptimeSLACalculator(startTimeBakti, endTime, timestamps, uptimeVals, exceptions)
+		if err != nil {
+			t.Fatalf("An Error should not be accoured: %v", err)
+		}
+		uptimeChronologies := calc.ExplainBakti1Uptime()
+		if uptimeChronologies == nil {
+			t.Fatalf("It should be not nil")
+		}
+		stateLen := map[int]int{}
+		for _, uptimeChronology := range uptimeChronologies {
+			state := uptimeChronology.Status
+			if _, ok := stateLen[state]; ok {
+				stateLen[state]++
+			} else {
+				stateLen[state] = 1
+			}
+		}
+		if _, ok := stateLen[slacalc.BaktiRunning]; !ok {
+			t.Fatalf("No running state in the states array")
+		}
+		if stateLen[slacalc.BaktiRunning] != 18 {
+			t.Errorf("The amount of running state is %v instead of 18", stateLen[slacalc.BaktiRunning])
+		}
+		if _, ok := stateLen[slacalc.BaktiLinkFailure]; !ok {
+			t.Fatalf("No link failure state in the states array")
+		}
+		if stateLen[slacalc.BaktiLinkFailure] != 4 {
+			t.Errorf("The amount of link failure state is %v instead of 4", stateLen[slacalc.BaktiLinkFailure])
+		}
+		if _, ok := stateLen[slacalc.BaktiPowerFailure]; !ok {
+			t.Fatalf("No power failure state in the states array")
+		}
+		if stateLen[slacalc.BaktiPowerFailure] != 4 {
+			t.Errorf("The amount of power failure state is %v instead of 4", stateLen[slacalc.BaktiPowerFailure])
+		}
+		if _, ok := stateLen[slacalc.BaktiOpen]; !ok {
+			t.Fatalf("No open state in the states array")
+		}
+		if stateLen[slacalc.BaktiOpen] != 4 {
+			t.Errorf("The amount of open state is %v instead of 4", stateLen[slacalc.BaktiPowerFailure])
+		}
+	}
+}
+
+func TestCalcBakti1Uptime(t *testing.T) {
+	// uptimeBaktiSeriesData
+	{
+		endTime := endTimeBakti
+		uptimeVals := []int{}
+		timestamps := []int64{}
+		exceptions := []bool{}
+		for _, val := range uptimeBaktiSeriesData {
+			uptimeVals = append(uptimeVals, val.Value)
+			timestamps = append(timestamps, val.Timestamp)
+			exceptions = append(exceptions, val.Exception)
+		}
+		calc, err := slacalc.NewUptimeSLACalculator(startTimeBakti, endTime, timestamps, uptimeVals, exceptions)
+		if err != nil {
+			t.Fatalf("An Error should not be accoured: %v", err)
+		}
+		bakti1Availability := calc.CalcBakti1Uptime()
+		if bakti1Availability == nil {
+			t.Fatalf("It should be not nil")
+		}
+		if math.Abs(bakti1Availability.Availability-.733333) > ACCURACY {
+			t.Errorf("Availability for uptime data is %.2f instead of 0.733333", bakti1Availability.Availability)
+		}
+	}
+	// AllUp
+	{
+		endTime := endTimeBakti
+		uptimeVals := []int{}
+		timestamps := []int64{}
+		exceptions := []bool{}
+		for _, val := range allUpUptimeSeriesData {
+			uptimeVals = append(uptimeVals, val.Value)
+			timestamps = append(timestamps, val.Timestamp)
+			exceptions = append(exceptions, val.Exception)
+		}
+		calc, err := slacalc.NewUptimeSLACalculator(timestamps[0], endTime, timestamps, uptimeVals, exceptions)
+		if err != nil {
+			t.Fatalf("An Error should not be accoured: %v", err)
+		}
+		bakti1Availability := calc.CalcBakti1Uptime()
+		if bakti1Availability == nil {
+			t.Fatalf("It should be not nil")
+		}
+		if math.Abs(bakti1Availability.Availability-1.) > ACCURACY {
+			t.Errorf("Availability for all up data is %.2f instead of 1.0", bakti1Availability.Availability)
+		}
+	}
+	// AllDown
+	{
+		endTime := endTimeBakti
+		uptimeVals := []int{}
+		timestamps := []int64{}
+		exceptions := []bool{}
+		for _, val := range allDownUptimeSeriesData {
+			uptimeVals = append(uptimeVals, val.Value)
+			timestamps = append(timestamps, val.Timestamp)
+			exceptions = append(exceptions, val.Exception)
+		}
+		calc, err := slacalc.NewUptimeSLACalculator(startTimeBakti, endTime, timestamps, uptimeVals, exceptions)
+		if err != nil {
+			t.Fatalf("An Error should not be accoured: %v", err)
+		}
+		bakti1Availability := calc.CalcBakti1Uptime()
+		if bakti1Availability == nil {
+			t.Fatalf("It should be not nil")
+		}
+		if math.Abs(bakti1Availability.Availability-0.0) > ACCURACY {
+			t.Errorf("Availability for all down data is %.2f instead of 0.0", bakti1Availability.Availability)
+		}
+	}
+}
